af11bb5796e10b075827f4821be34a961d13f2ea
yyt123456 <2997353803@qq.com>
2021-03-18 18:23:09 +0800

    feat: sql语句

--- router/user.js
+++ router/user.js
@@ -2,13 +2,16 @@ const express = require('express')
 const Result = require('../modules/result')
 const {login} = require('../services/user')
 const router = express.Router()
+const { md5 } = require('../utils/index')
+const { PWD_SALT } = require('../utils/constant')
 
 
 router.get('/info', function (req, res, next) {
   res.json('user info...')
 })
 router.post('/login', function (req, res, next) {
-  const {username, password} = req.body
+  let {username, password} = req.body
+  password = md5(`${password}${PWD_SALT}`)
   login(username, password).then(user => {
     if (!user || user.length === 0) {
       new Result('登录失败').fail(res)
